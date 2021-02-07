6e6f5c141e47ddbdaf2825330fb5b55b827f4fc3
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-07 17:20:29 -0300

    Tentativa 2 (deu certo)

--- lem_irrefutavel.v
+++ lem_irrefutavel.v
@@ -5,5 +5,7 @@ Theorem lem_irrefutavel :
 lem -> forall (p : Prop), ~~(p \/ ~p).
 Proof.
   intros Hlem p.
-  unfold not.
   intro Hn_p_or_np.
+  unfold not in Hn_p_or_np.
+  apply (Hn_p_or_np (Hlem p)).
+Qed.
