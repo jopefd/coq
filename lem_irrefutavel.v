038105fdd817dbccca941090dc21bce6be47edbb
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-07 17:07:10 -0300

    Tentativa 1

--- lem_irrefutavel.v
+++ lem_irrefutavel.v
@@ -1,1 +1,9 @@
+Definition lem : Prop :=
+forall (p : Prop), p \/ (~p).
 
+Theorem lem_irrefutavel :
+lem -> forall (p : Prop), ~~(p \/ ~p).
+Proof.
+  intros Hlem p.
+  unfold not.
+  intro Hn_p_or_np.
