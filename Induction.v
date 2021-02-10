d8e2d77ae659f4deff1556937efdb25fa9fea05c
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-09 21:55:32 -0300

    Theorem mult_0_r

--- Induction.v
+++ Induction.v
@@ -204,7 +204,14 @@ Proof.
 Theorem mult_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intro n.
+  induction n as [ | n'].
+  { simpl.
+    reflexivity.  }
+  { simpl.
+    rewrite -> IHn'.
+    reflexivity.  }
+Qed.
 
 Theorem plus_n_Sm : forall n m : nat,
   S (n + m) = n + (S m).
