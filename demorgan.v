acf29fdfec0f4887dc83cf92cadf8b457dd8aa7d
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-12 08:59:11 -0300

    so sucesso 4

--- demorgan.v
+++ demorgan.v
@@ -33,3 +33,33 @@ Proof.
   - apply Hnq in Hq.
     assumption.
 Qed.
+
+Definition lem : Prop :=
+forall (p : Prop), (p \/ ~p).
+
+Theorem demorgan3 :
+lem -> forall (p q : Prop), ~(p /\ q) -> (~q \/ ~p).
+Proof.
+  intros Hlem p q.
+  unfold not.
+  intro Hnnpandq.
+  destruct (Hlem p) as [Hp | Hnp].
+  - destruct (Hlem q) as [Hq | Hnq].
+    + assert (Hpandq: p /\ q).
+      * split.
+        -- assumption.
+        -- assumption.
+      * left.
+        intro Hq'.
+        apply Hnnpandq in Hpandq.
+        assumption.
+    + left.
+      intro Hq.
+      unfold not in Hnq.
+      apply Hnq in Hq.
+      assumption.
+  - right.
+    intro Hp.
+    apply Hnp in Hp.
+    assumption.
+Qed.
