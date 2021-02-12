d16ae573cb7d56fe99c4975f94f9b65b728c3966
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-12 09:13:25 -0300

    no baile nois eh midia 1

--- distr_or_and.v
+++ distr_or_and.v
@@ -1,1 +1,16 @@
-
+Theorem distr1_or_and :
+forall (p q r: Prop), p /\ (q \/ r) -> (p /\ q) \/ (p /\ r).
+Proof.
+  intros p q r.
+  intro Hpandqorr.
+  destruct Hpandqorr as [Hp Hqorr].
+  destruct Hqorr as [Hq | Hr].
+  - left.
+    split.
+    + assumption.
+    + assumption.
+  - right.
+    split.
+    + assumption.
+    + assumption.
+Qed.
