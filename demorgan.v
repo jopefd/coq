1c9bd4444041f21dab8901d8ee81238c04ea8b51
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-02-12 08:45:42 -0300

    so sucesso 2

--- demorgan.v
+++ demorgan.v
@@ -18,3 +18,18 @@ Proof.
     + apply Hnporq in Hporq.
       assumption.
 Qed.
+
+Theorem demorgan2 :
+forall (p q : Prop), (~p /\ ~q) -> ~(p \/ q).
+Proof.
+  intros p q.
+  unfold not.
+  intro Hnpandnq.
+  intro Hporq.
+  destruct Hnpandnq as [Hnp Hnq].
+  destruct Hporq as [Hp | Hq].
+  - apply Hnp in Hp.
+    assumption.
+  - apply Hnq in Hq.
+    assumption.
+Qed.
